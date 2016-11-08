f5c7b46aa9a2186f4c81a71d871b733170c73c62
Joakim Loxdal <joakim.loxdal@ibm.com>
2016-11-08 15:01:38 +0100

    font-size change, placeholder added

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,6 @@ var app = angular.module('kpi-app', []);
 app.controller('kpi-controller', function($scope){
     $scope.data = kpiData;
     $scope.result = 0;
-    $scope.kronor = 0;
     $scope.calcWorth = function(){
         var converter = $scope.toyear/$scope.fromyear;
         $scope.result = Math.floor($scope.kronor*converter*100)/100;
